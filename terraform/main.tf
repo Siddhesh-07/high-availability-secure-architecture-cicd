19404cb839b3aedb5551ab957271d1690dcab8d0
Siddhesh-07 <siddheshgk07@gmail.com>
2026-07-11 17:44:38 +0530

    added 2nd public subnet for ALB

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-# Public Subnet (for ALB)
+# Public Subnet (for ALB) - AZ-a
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr
@@ -40,7 +40,19 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.project_name}-public-subnet"
+    Name = "${var.project_name}-public-subnet-1"
+  }
+}
+
+# Public Subnet (for ALB) - AZ-b 
+resource "aws_subnet" "public_2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-2"
   }
 }
 
@@ -105,6 +117,11 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
 
