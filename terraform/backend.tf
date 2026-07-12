b4413be05e4581e78b2cc7657205691256a815b9
Siddhesh-07 <siddheshgk07@gmail.com>
2026-07-12 19:47:51 +0530

    starting pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,11 +2,11 @@
 
 terraform {
    backend "s3" {
-     bucket         = "hasa-app-terraform-remote-state-siddhesh"
-     key            = "prod/terraform.tfstate"
+    bucket         = "hasa-app-terraform-remote-state-siddhesh"
+    key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "hasa-app-terraform-locks"
-     encrypt        = true
+    encrypt        = true
    }  
 }
 
